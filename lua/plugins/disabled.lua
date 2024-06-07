440283b57288692a551e8d7206bb4f29b665b0e7
Maciej Spiechowicz <spm1fr@bosch.com>
2024-06-07 13:43:42 +0200

    remove the nvim surround and replace with vim sandwich, disable folke flash

--- lua/plugins/disabled.lua
+++ lua/plugins/disabled.lua
@@ -5,4 +5,5 @@ return {
   { "catppuccin/nvim", enabled = false },
   { "echasnovski/mini.indentscope", enabled = false },
   { "rcarriga/nvim-notify", enabled = false },
+  { "folke/flash.nvim", enabled = false },
 }
