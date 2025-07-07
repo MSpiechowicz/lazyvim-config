ea57f08215d9682a568616f29c3f19237050a72b
Maciej Spiechowicz <spm1fr@bosch.com>
2025-07-07 10:23:30 +0200

    Update the conform styles

--- lua/plugins/conform.lua
+++ lua/plugins/conform.lua
@@ -2,13 +2,13 @@ return {
   "stevearc/conform.nvim",
   opts = {
     formatters_by_ft = {
-      typescript = { { "prettierd", "prettier" } },
-      typescriptreact = { { "prettierd", "prettier" } },
-      javascript = { { "prettierd", "prettier" } },
-      javascriptreact = { { "prettierd", "prettier" } },
-      json = { { "prettierd", "prettier" } },
-      html = { { "prettierd", "prettier" } },
-      css = { { "prettierd", "prettier" } },
+      typescript = { "prettierd", "prettier" },
+      typescriptreact = { "prettierd", "prettier" },
+      javascript = { "prettierd", "prettier"  },
+      javascriptreact = { "prettierd", "prettier" },
+      json = { "prettierd", "prettier" },
+      html = { "prettierd", "prettier" },
+      css = { "prettierd", "prettier" },
     },
   },
 }
